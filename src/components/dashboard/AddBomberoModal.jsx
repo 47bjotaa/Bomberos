b497771821985f5cad05f0b9fa8706da87141c9b
NicolasContreras <nicolascontrerasrios05@gmail.com>
2026-05-26 15:09:47 -0400

    Agregar bombero5

--- src/components/dashboard/AddBomberoModal.jsx
+++ src/components/dashboard/AddBomberoModal.jsx
@@ -141,6 +141,7 @@ function AddBomberoModal({ onClose, onAdded }) {
                 style={{ background: palette.bg, borderColor: palette.border, color: palette.text }}
               >
                 <option value="" disabled>Seleccionar cargo</option>
+                <option value="Bombero">Bombero</option>
                 <option value="Voluntario">Voluntario</option>
                 <option value="Capitan">Capitan</option>
                 <option value="Teniente">Teniente</option>
